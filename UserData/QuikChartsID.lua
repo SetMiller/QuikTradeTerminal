381b299d5109f39d20fc113724a273d72c86b9c9
SetMiller <setmiller@gmail.com>
2021-03-26 00:49:47 +0300

    update

--- UserData/QuikChartsID.lua
+++ UserData/QuikChartsID.lua
@@ -16,36 +16,49 @@ function QuikChartsID:new()
         ['CLASSCODE']   = "SPBFUT",       -- класс фьючерса  
       },
       charts = {
-        ['stockDayChartId']     = "SBER_D_STOCK_PRICE",       -- id графика акции
-        ['stockHourChartId']    = "SBER_H_STOCK_PRICE",       -- id графика акции
-        ['futuresDayChartId']   = "SBER_D_FUTURES_PRICE",     -- id графика фьючерса
-        ['futuresHourChartId']  = "SBER_H_FUTURES_PRICE",     -- id графика фьючерса
+        ['stockD']      = "SBER_D_STOCK_PRICE",       -- id графика акции
+        ['stockH']      = "SBER_H_STOCK_PRICE",       -- id графика акции
+        ['futuresD']    = "SBER_D_FUTURES_PRICE",     -- id графика фьючерса
+        ['futuresH']    = "SBER_H_FUTURES_PRICE",     -- id графика фьючерса
+      }
     }
 
   local Public = {}
 
   -- 
-  -- Метод getStockID() реализует доступ к идентификаторам акции
+  -- Метод get() реализует доступ к идентификаторам графиков для акции и фьючерса
   -- 
-  function getStockID()
-    return Private.chartsID.stock
-  end
-
-  -- 
-  -- Метод getFuturesID() реализует доступ к идентификаторам фьючерса
-  -- 
-  function getFuturesID()
-    return Private.chartsID.futures
-  end
-
-  -- 
-  -- Метод getQuikChartsID() реализует доступ к идентификаторам графиков для акции и фьючерса
-  -- 
-  function getQuikChartsID()
-    return Private.chartsID.charts
+  -- @param idType string
+  -- @param chartType string
+  function Public:get(idType, chartType)
+    if type(idType) ~= 'string' then error(("bad argument idType: QuikChartsID -> get() (string expected, got %s)"):format(type(idType)), 2) end
+    local isFound = false
+    for ik, iv in pairs(Private.chartsID) do
+      if ik == idType then
+        if ik == 'charts' then
+          if type(chartType) ~= 'string' then error(("bad argument chartType: QuikChartsID -> get() (string expected, got %s)"):format(type(chartType)), 2) end
+          for jk, jv in pairs(iv) do
+            if jk == chartType then 
+              isFound = true
+              return jv
+            end
+          end
+          if not isFound then error(("bad argument, there is no \"%s\" chartType: QuikChartsID -> get())"):format(chartType), 2) end
+        else
+          isFound = true
+          return iv
+        end
+      end
+    end
+    if not isFound then error(("bad argument, there is no \"%s\" idType: QuikChartsID -> get())"):format(idType), 2) end
   end
 
   setmetatable(Public, self)
   self.__index = self
   return Public
 end
+
+
+
+
+-- TESTED
